dcd2f23aeeec7f01cd902337157aa96a3cb6f5ac
Hetvi Tank <hetvi.tank@aeonx.digital>
2024-02-09 12:50:28 +0530

    chnage in roject view page

--- ai_project/ai_project/views.py
+++ ai_project/ai_project/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from django.shortcuts import render
 def home(request):
     # Add any necessary logic here
     return render(request, 'index.html')
